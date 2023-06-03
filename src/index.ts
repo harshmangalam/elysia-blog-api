ef54d2708dee008221b93775a05f94977c1daefa
harshmangalam <harshdev8218@gmail.com>
2023-06-03 19:58:04 +0530

    added prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Elysia } from "elysia";
+import { prisma } from "./lib/prisma";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(8080);
+const app = new Elysia()
+  .use((app) => app.state("prisma", prisma))
+  .get("/", async ({ store }) => {
+    return "Hello";
+  })
+  .listen(8080);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
