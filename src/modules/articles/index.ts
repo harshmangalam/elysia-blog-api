97a023e7cec0ff6206d9861e7dec4242b9660551
harshmangalam <harshdev8218@gmail.com>
2023-06-04 21:26:13 +0530

    fetch all articles

--- src/modules/articles/index.ts
+++ src/modules/articles/index.ts
@@ -4,35 +4,47 @@ import { prisma } from "~libs";
 
 export const articles = (app: Elysia) =>
   app.group("/articles", (app) =>
-    app.use(isAuthenticated).post(
-      "/",
-      async ({ body, user }) => {
-        const article = await prisma.article.create({
-          data: {
-            authorId: user!.id,
-            ...body,
-          },
-        });
-
+    app
+      .get("/", async () => {
+        const articles = await prisma.article.findMany();
         return {
           success: true,
-          message: "Article created",
+          message: "Fetch articles",
           data: {
-            article,
+            articles,
           },
         };
-      },
-      {
-        body: t.Object({
-          title: t.String(),
-          bodyMarkdown: t.String(),
-          published: t.Optional(t.Boolean({ default: false })),
-          mainImage: t.Optional(t.String()),
-          canonicalUrl: t.Optional(t.String()),
-          description: t.String(),
-          tags: t.String(),
-          organisationId: t.Optional(t.String()),
-        }),
-      }
-    )
+      })
+      .use(isAuthenticated)
+      .post(
+        "/",
+        async ({ body, user }) => {
+          const article = await prisma.article.create({
+            data: {
+              authorId: user!.id,
+              ...body,
+            },
+          });
+
+          return {
+            success: true,
+            message: "Article created",
+            data: {
+              article,
+            },
+          };
+        },
+        {
+          body: t.Object({
+            title: t.String(),
+            bodyMarkdown: t.String(),
+            published: t.Optional(t.Boolean({ default: false })),
+            mainImage: t.Optional(t.String()),
+            canonicalUrl: t.Optional(t.String()),
+            description: t.String(),
+            tags: t.String(),
+            organisationId: t.Optional(t.String()),
+          }),
+        }
+      )
   );
